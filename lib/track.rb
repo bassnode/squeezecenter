e2f0c3256295ffdb2cbed60b43429dc1f62068d1
Ed Hickey <ed@ed-new-laptop.local>
2009-01-10 17:52:30 -0600

    forgot this one on last commit - used to be named song.rb

---
+++ lib/track.rb
@@ -0,0 +1,11 @@
+class Track < RubySqueeze::Item
+  attr_reader :artist, :album, :genre
+  
+  def [](attribute)
+    attributes[attribute]
+  end
+
+  def by_artist(artist)
+    invoke "tracks 0 5000 artist_id:#{artist.id}"
+  end
+end
